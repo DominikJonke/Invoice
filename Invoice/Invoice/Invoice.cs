2cd31421260b14c80e91f32d4eb2cb4a0708638f
D J <dominik.jonke@outlook.com>
2020-02-13 14:46:22 +0100

    New class invoice

---
+++ Invoice/Invoice/Invoice.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Invoice
+{
+    class Invoice
+    {
+
+    }
+}
---
+++ Invoice/Invoice/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Invoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
