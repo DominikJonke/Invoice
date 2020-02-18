a129c2943e2215a8e900eff17cc4464596af7d9d
D J <dominik.jonke@outlook.com>
2020-02-19 00:27:06 +0100

    Invoice cable and drum in euro

--- Invoice/Invoice/Invoice.cs
+++ Invoice/Invoice/Invoice.cs
@@ -4,12 +4,54 @@ namespace Invoice
 {
     public static class Invoice
     {
-        const int cableDrum = 500;          // per meter
-        const int perMeter = 2;             // Euro
-        const int perMeterStarted = 3;      // Euro
-        const int income = 60;              // Euro/Hour
-        const int extraHours = 90;          // 50 %
+        const int fullDrum = 500;
+        public static double FullLength(int drum)
+        {
+            int cableSum = 0;
+            int diffDrum = 0;
 
-        public
+            if(drum > 0 && drum % fullDrum != 0)
+            {
+                diffDrum = (drum % fullDrum);
+                cableSum = (drum - diffDrum);
+
+                return (diffDrum * 3) + (cableSum * 2);
+            }
+            
+            else if (drum > 0 && drum % fullDrum == 0)
+            {
+                cableSum = (drum) * 2;
+                
+                return cableSum;
+            }
+
+            return -1;
+        }
+
+        public static double ConsumedDrums(int cabeldrum)
+        {
+            int drums = 0;
+            int diff = 0;
+
+            if (cabeldrum > 0)
+            {
+                diff = cabeldrum % fullDrum;
+                drums = (cabeldrum - diff) / fullDrum;
+            }
+
+            return drums;
+        }
+
+        public static double ConsumedRemainingCables(int drums)
+        {
+            int remainingCable = 0;
+
+            if (drums > 0 && drums % fullDrum != 0)
+            {
+                remainingCable = drums % fullDrum;
+            }
+
+            return remainingCable;
+        }
     }
 }
--- Invoice/Invoice/Program.cs
+++ Invoice/Invoice/Program.cs
@@ -6,11 +6,22 @@ namespace Invoice
     {
         static void Main(string[] args)
         {
+            int cabelLength = 0;
+
             Console.WriteLine();
             Console.WriteLine("\t\t Invoice Calculator");
             Console.WriteLine();
 
+            Console.WriteLine("Cabel Lenght: ");
+            cabelLength = Convert.ToInt32(Console.ReadLine());
 
+            Console.Write($"\t Cable costs: \t\t\t\t {Invoice.FullLength(cabelLength)} euro\n");
+            Console.Write($"\t\t {Invoice.ConsumedDrums(cabelLength)} drums: \t\t {Invoice.ConsumedDrums(cabelLength * 500 * 2)} euro\n");
+            Console.Write($"\t\t {Invoice.ConsumedRemainingCables(cabelLength)} meters: \t\t\t {Invoice.ConsumedRemainingCables(cabelLength) * 3} euro\n");
+            
+
+
+            Console.ReadKey();
         }
     }
 }
