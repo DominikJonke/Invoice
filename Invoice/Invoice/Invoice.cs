d0d08de379b48436393a6c80ab0e5bccdfd99987
D J <dominik.jonke@outlook.com>
2020-02-20 17:03:11 +0100

    Different static methods

--- Invoice/Invoice/Invoice.cs
+++ Invoice/Invoice/Invoice.cs
@@ -4,54 +4,94 @@ namespace Invoice
 {
     public static class Invoice
     {
-        const int fullDrum = 500;
-        public static double FullLength(int drum)
+        public static int meter = 0;
+        public static int hour = 0;
+
+      
+        public static void GetLenght()
         {
-            int cableSum = 0;
-            int diffDrum = 0;
-
-            if(drum > 0 && drum % fullDrum != 0)
-            {
-                diffDrum = (drum % fullDrum);
-                cableSum = (drum - diffDrum);
-
-                return (diffDrum * 3) + (cableSum * 2);
-            }
-            
-            else if (drum > 0 && drum % fullDrum == 0)
-            {
-                cableSum = (drum) * 2;
-                
-                return cableSum;
-            }
-
-            return -1;
+            Console.WriteLine();
+            Console.WriteLine("\t\t\t Company Invoice");
+            Console.WriteLine();
+            Console.Write("\t How much meter: ");
+            meter = Convert.ToInt32(Console.ReadLine());
         }
 
-        public static double ConsumedDrums(int cabeldrum)
+        public static void GetHour()
         {
-            int drums = 0;
-            int diff = 0;
-
-            if (cabeldrum > 0)
-            {
-                diff = cabeldrum % fullDrum;
-                drums = (cabeldrum - diff) / fullDrum;
-            }
-
-            return drums;
+            Console.Write("\t How much hours: ");
+            hour = Convert.ToInt32(Console.ReadLine());
         }
 
-        public static double ConsumedRemainingCables(int drums)
+        public static void CalcDrum()
         {
-            int remainingCable = 0;
+            int sumDrum = 0;
+            int sumStartedDrum = 0;
 
-            if (drums > 0 && drums % fullDrum != 0)
+            if (meter > 0)
             {
-                remainingCable = drums % fullDrum;
+                sumStartedDrum = meter % 500;
+                sumDrum = meter / 500;
+
+                Console.WriteLine($"\n\t Cable costs: \t\t\t {(sumDrum * 2 * 500) + (sumStartedDrum * 3)} \t Euro");
+                Console.WriteLine($"\t\t {sumDrum} Drums:\t\t {sumDrum * 2 * 500} \t Euro");
+                Console.WriteLine($"\t\t {sumStartedDrum} Meter:\t\t {sumStartedDrum * 3} \t Euro ");
             }
 
-            return remainingCable;
+            else
+            {
+                Console.WriteLine("Try again!");
+            }
+        }
+
+        public static void CalcLabourCosts()
+        {
+            int sumHour = 0;
+            int sumOvertime = 0;
+
+            if (hour > 0 && hour <= 40)
+            {
+                sumHour = hour;
+            }
+
+            else if (hour > 0 && hour > 40)
+            {
+                sumHour = 40;
+                sumOvertime = hour - 40; 
+            }
+
+            Console.WriteLine($"\n\t Hour costs: \t\t\t {(sumHour * 60) + (sumOvertime * 90)} \t Euro");
+            Console.WriteLine($"\t\t {sumHour} hours:\t\t {sumHour * 60} \t Euro");
+            Console.WriteLine($"\t\t {sumOvertime} overtime:\t\t {sumOvertime * 90} \t Euro ");
+        }
+
+        public static void CalcTotal()
+        {
+            int sumDrum = 0;
+            int sumStartedDrum = 0;
+            int sumHour = 0;
+            int sumOvertime = 0;
+            int total = 0;
+
+            if(meter > 0 || hour > 0)
+            {
+                sumDrum = meter / 500;
+                sumStartedDrum = meter % 500;
+
+                if (hour <= 40)
+                {
+                    sumHour = hour;
+                }
+                else if (hour > 40)
+                {
+                    sumHour = 40;
+                    sumOvertime = hour - 40;
+                }
+
+                total = (sumDrum * 2 * 500) + (sumStartedDrum * 3) + (sumHour * 60) + (sumOvertime * 90); 
+            }
+
+            Console.WriteLine($"\n\t Total: \t\t\t {total} \t Euro");
         }
     }
 }
--- Invoice/Invoice/Program.cs
+++ Invoice/Invoice/Program.cs
@@ -6,20 +6,12 @@ namespace Invoice
     {
         static void Main(string[] args)
         {
-            int cabelLength = 0;
+            Invoice.GetLenght();
+            Invoice.GetHour();
 
-            Console.WriteLine();
-            Console.WriteLine("\t\t Invoice Calculator");
-            Console.WriteLine();
-
-            Console.WriteLine("Cabel Lenght: ");
-            cabelLength = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write($"\t Cable costs: \t\t\t\t {Invoice.FullLength(cabelLength)} euro\n");
-            Console.Write($"\t\t {Invoice.ConsumedDrums(cabelLength)} drums: \t\t {Invoice.ConsumedDrums(cabelLength * 500 * 2)} euro\n");
-            Console.Write($"\t\t {Invoice.ConsumedRemainingCables(cabelLength)} meters: \t\t\t {Invoice.ConsumedRemainingCables(cabelLength) * 3} euro\n");
-            
-
+            Invoice.CalcDrum();
+            Invoice.CalcLabourCosts();
+            Invoice.CalcTotal();
 
             Console.ReadKey();
         }
